570322a52db1b8db390dff4d7e32b87274a719f0
Antony Brahams Paredes Paulino <brahamscompany@gmail.com>
2024-11-26 18:38:59 -0500

    asda

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://BrahamsCode.github.io/vivatech'
+  base: '/',  // Esto es importante para Vercel
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
 })
